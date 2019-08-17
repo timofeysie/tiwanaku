f77926dabb3d8812f8cb5bb350317dc2014dcadd
Timothy Curchod <timofeyc@hotmail.com>
2019-08-17 17:41:03 +1000

    #4 completing dark mode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,8 +4,35 @@ import { IAppState } from './store/state/app.state';
 import { GetConfig } from './store/actions/config.actions';
 import { selectConfig } from './store/selectors/config.selector';
 import { selectEntityList } from './store/selectors/entity.selector';
-import { EntitiesComponent } from './components/entities/entities.component';
 import { Observable } from 'rxjs';
+import { ThemeService } from './services/theme.service';
+
+const themes = {
+    autumn: {
+      primary: '#F78154',
+      secondary: '#4D9078',
+      tertiary: '#B4436C',
+      light: '#FDE8DF',
+      medium: '#FCD0A2',
+      dark: '#B89876'
+    },
+    night: {
+      primary: '#8CBA80',
+      secondary: '#FCFF6C',
+      tertiary: '#FE5F55',
+      medium: '#BCC2C7',
+      dark: '#F7F7FF',
+      light: '#495867'
+    },
+    neon: {
+      primary: '#39BFBD',
+      secondary: '#4CE0B3',
+      tertiary: '#FF5E79',
+      light: '#F4EDF2',
+      medium: '#B682A5',
+      dark: '#34162A'
+    }
+};
 
 @Component({
   selector: 'app-root',
@@ -18,7 +45,9 @@ export class AppComponent implements OnInit {
   error$: Observable<any>;
   entities$ = this._store.pipe(select(selectEntityList));
 
-  constructor(private _store: Store<IAppState>) {
+  constructor(
+      private _store: Store<IAppState>,
+      private theme: ThemeService) {
       this.error$ = _store.pipe(select('error'));
   }
 
